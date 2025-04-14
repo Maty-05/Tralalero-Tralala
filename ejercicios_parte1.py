bce31a061bcb2fec7ac037594b22271c9e389b85
Maty-05 <matiasjeldres2005@gmail.com>
2025-04-14 13:47:25 -0400

    Mi primer Commit

---
+++ ejercicios_parte1.py
@@ -0,0 +1,31 @@
+Nombre = "Matias"
+Edad = 19
+Cancion_Favorita = "Beat_it"
+
+Linea1 = "They told him, Don't you ever come around here"
+Linea2 = "Don't wanna see your face, you better disappear"
+Linea3 = "The fire's in their eyes and their words are really clear"
+Linea4 = "So beat it, just beat it"
+
+print(f"Hola, Me llamó {Nombre} y tengo \n{Edad} Años")
+print(f"Mi canción favorita es {Cancion_Favorita}")
+print("")
+print("\   /")
+print("O   O")
+print("  w  ")
+print("-----")
+print("")
+print("5")
+print("4")
+print("3")
+print("2")
+print("1")
+print("Y Fuera¡")
+print("")
+print("")
+
+print(Linea1)
+print(Linea2)
+print(Linea3)
+print(Linea4)
+print("")
