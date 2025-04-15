b5fe09813e3b22a637add49b52bf0afcbbca1ccd
Maty-05 <matiasjeldres2005@gmail.com>
2025-04-15 15:00:30 -0400

    nuevas funciones booleanas

--- Inicio.py
+++ Inicio.py
@@ -1,9 +1,10 @@
-# Este es mi primer programa en Python
-#giprint("¡Hola mundo!")
-#print("Bienvenidos al taller de Python")
-# Podemos imprimir números directamente
-#print(2023)
-# O hacer operaciones basicas
-#print(20+45)
-#print(True)
-#print(False)
+print("¡Hola mundo!")
+print("Bienvenidos al taller de Python")
+
+print(2023)
+
+
+print(20+45)
+print(5<4)
+print(4>5)
+print(5 != 5)
