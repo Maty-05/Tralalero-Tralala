5b9abc84ad7e0091566fbad7dead9cc4f0a6c0d7
Maty-05 <matiasjeldres2005@gmail.com>
2025-04-16 10:49:36 -0400

    Nuevos documentos

---
+++ Ejercicios_Dia_16.py
@@ -0,0 +1,48 @@
+#Ejercicio 1:
+nombre = "Matias"
+edad = 19
+estatura = 1.79
+print("Nombre: ", nombre)
+print("Edad: ", edad)
+print("Estatura: ", estatura)
+#Ejercicio 2:
+año_nacimiento = 2005
+año_actual = 2025
+edad_actual = año_actual - año_nacimiento
+print("Tú Edad es: ", edad_actual)
+
+#Nivel Medio:
+
+#Ejercicio 3:
+precio_original = 10000
+descuento = 0.15
+precio_final = precio_original * (1 - descuento)
+print("El precio final es: ", precio_final)
+
+#ejercicio 4:
+edad = edad_actual
+meses = edad * 12
+semanas = meses * 4
+dias = semanas * 7
+horas = dias * 24
+minutos = horas * 60
+segundos = minutos * 60
+print("Tu edad en meses es: ", meses)
+print("Tu edad en semanas es: ", semanas)
+print("Tu edad en dias es: ", dias)
+print("Tu edad en horas es: ", horas)
+print("Tu edad en minutos es: ", minutos)
+print("Tu edad en segundos es: ", segundos)
+
+#Ejercicio 5:
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.0
+promedio = (nota1 + nota2 + nota3) / 3
+print("El promedio de las notas es: ", promedio)
+if promedio >= 4.0:
+    print("Aprobado")
+else:
+    print("Reprobado")
+
+
